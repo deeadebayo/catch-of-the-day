ef5c175d5e9a286d93499f1f2e642bb11b98d97f
Dee Adebayo <deeadebayo@gmail.com>
2020-12-07 02:06:12 -0500

    added routing and smaller components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
-export default class StorePicker extends React.Component {
+class StorePicker extends React.Component {
 	render() {
-		return <p>Hello!</p>;
+		return (
+			<form className='store-selector'>
+				<h2>Please Enter a Store</h2>
+				<input
+					type='text'
+					required
+					placeholder='Store Name'
+					defaultValue={getFunName()}
+				/>
+				<button type='submit'>Visit Store → </button>
+			</form>
+		);
 	}
 }
+
+export default StorePicker;
