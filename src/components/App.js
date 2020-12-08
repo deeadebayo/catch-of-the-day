72b9e0cb0170a9936adf9d1df0609169ee57cf97
Dee Adebayo <deeadebayo@gmail.com>
2020-12-08 01:24:25 -0500

    added in state

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,35 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+	state = {
+		fishes: {},
+		order: {},
+	};
+
+	addFish = (fish) => {
+		const fishes = { ...this.state.fishes };
+		fishes[`fishes${Date.now()}`] = fish;
+		this.setState({
+			fishes,
+		});
+	};
+	loadSampleFishes = () => {
+		this.setState({ fishes: sampleFishes });
+	};
 	render() {
 		return (
 			<div className='catch-of-the-day'>
 				<div className='menu'>
-					<Header tagline='Fresh Seasfood Market' age={500} />
+					<Header tagline='Fresh Seafood Market' age={500} />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory
+					addFish={this.addFish}
+					loadSampleFishes={this.loadSampleFishes}
+				/>
 			</div>
 		);
 	}
